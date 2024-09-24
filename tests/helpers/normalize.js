f964aadc0a174258d7d217ccedf50dbf23b239cf
LingyuCoder <lingyucoder@gmail.com>
2024-09-24 11:56:56 +0800

    feat: add rspack-dev-server eco ci

--- tests/helpers/normalize.js
+++ tests/helpers/normalize.js
@@ -3,16 +3,28 @@ const path = require("path");
 const CURRENT_CWD = process.cwd();
 const ROOT = path.resolve(__dirname, "../../");
 
+let RSPACK = "";
+
+try {
+	RSPACK = path.dirname(require.resolve("@rspack/core-canary/package.json"));
+} catch(e) {
+	RSPACK = path.dirname(require.resolve("@rspack/core/package.json"));
+}
+
 const normalize = str => {
 	let normalizedStr = str.replace(/(\\)+/g, "/");
 	
 	normalizedStr = normalizedStr.split(
-		CURRENT_CWD.replace(/(\\)+/g, "/")
-	).join("<cwd>");
+		RSPACK.replace(/(\\)+/g, "/")
+	).join("<rspack>");
 
 	normalizedStr = normalizedStr.split(
 		ROOT.replace(/(\\)+/g, "/")
 	).join("<root>");
+
+	normalizedStr = normalizedStr.split(
+		CURRENT_CWD.replace(/(\\)+/g, "/")
+	).join("<cwd>");
 
 	normalizedStr = normalizedStr.replace(/:\d+:\d+/g, ":<line>:<row>");
 	normalizedStr = normalizedStr.replace(

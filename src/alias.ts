07f618aeba3a1767a268dd4858270127468dedc1
LingyuCoder <lingyucoder@gmail.com>
2024-09-24 22:32:20 +0800

    fix: alias of require.resolve

--- src/alias.ts
+++ src/alias.ts
@@ -1,4 +1,5 @@
-const RESOLVER_MAP: Record<string, typeof require.resolve> = {};
+const Module = require("module");
+const RESOLVER_MAP: Record<string, typeof Module._resolveFilename> = {};
 export const addResolveAlias = (
 	name: string,
 	aliasMap: Record<string, string>
@@ -7,17 +8,13 @@ export const addResolveAlias = (
 	if (RESOLVER_MAP[modulePath]) {
 		throw new Error(`Should not add resolve alias to ${name} again.`);
 	}
-	const m = require.cache[modulePath];
-	if (!m) {
-		throw new Error("Failed to resolve webpack-dev-server.");
-	}
-	RESOLVER_MAP[modulePath] = m.require.resolve;
-	m.require.resolve = ((id: string, options?: any) =>
-		aliasMap[id] ||
-		RESOLVER_MAP[modulePath]!.apply(m.require, [
-			id,
-			options
-		])) as typeof require.resolve;
+	RESOLVER_MAP[modulePath] = Module._resolveFilename;
+	Module._resolveFilename = Module._resolveFilename = (request: string, mod: NodeModule, ...args: unknown[]) => {
+			if (mod.filename === modulePath && aliasMap[request]) {
+				return aliasMap[request];
+			}
+			return RESOLVER_MAP[modulePath](request, mod, ...args);
+	};
 };
 
 export const removeResolveAlias = (name: string) => {
@@ -30,7 +27,7 @@ export const removeResolveAlias = (name: string) => {
 		throw new Error("Failed to resolve webpack-dev-server");
 	}
 	if (RESOLVER_MAP[modulePath]) {
-		m.require.resolve = RESOLVER_MAP[modulePath]!;
+		Module._resolveFilename = RESOLVER_MAP[modulePath]!;
 		delete RESOLVER_MAP[modulePath];
 	}
 };
